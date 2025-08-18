628e3d5c1ae6111236d6cd23bde18e0705b24380
PedroLFPrado <pedro.lfprado@gmail.com>
2025-08-18 13:58:11 -0300

    conexao com banco feita

---
+++ CrudEMnet/Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+
+
+namespace CrudEMnet.Data
+{
+    public class AppDbContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public AppDbContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = Configuration.GetConnectionString("conexao");
+            optionsBuilder.UseNpgsql(connectionString);
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
---
+++ CrudEMnet/Data/Produto.cs
@@ -0,0 +1,34 @@
+﻿namespace CrudEMnet.Data
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    [Table("produto")]
+    public class Produto
+    {
+        [Key] // marca como chave primária
+        [Column("codigo")] 
+        public int Codigo { get; set; }
+
+        [Column("descricao")]
+        public string Descricao { get; set; }
+
+        [Column("datavalidade")]
+        public DateTime DataValidade { get; set; }
+
+        [Column("preco")]
+        public double Preco { get; set; }
+
+        [Column("taxalucro")]
+        public double TaxaLucro { get; set; }
+
+        // Não mapeados (calculados em memória)
+        [NotMapped]
+        public double PrecoFinal => Preco + (Preco * TaxaLucro / 100);
+
+        [NotMapped]
+        public int PrazoValidade => (DataValidade - DateTime.Now).Days;
+    }
+
+}
